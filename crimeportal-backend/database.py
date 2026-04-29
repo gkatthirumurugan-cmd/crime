3057ebac483270d73475bba464fcb1d64ca34ab4
Thiru Murugan <gkatthirumurugan@gmail.com>
2026-04-29 22:51:47 +0530

    Backend setup for Vercel

--- crimeportal-backend/database.py
+++ crimeportal-backend/database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "mysql+pymysql://root:agal%402004@localhost:3306/crimeportal"
+DATABASE_URL = os.getenv("mysql+pymysql://root:agal%402004@localhost:3306/crimeportal")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False)
